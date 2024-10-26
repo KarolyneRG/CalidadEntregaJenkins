9c34307637f6d5db60c18a60b1201f0209f35975
KarolyneRG <karolyne.renteriag@unac.edu.co>
2024-10-25 19:28:41 -0500

    pruebaPlugins

--- src/main/java/edu/unac/Main.java
+++ src/main/java/edu/unac/Main.java
@@ -8,7 +8,7 @@ import edu.unac.exception.RepeatedCharacterException;
 import java.security.SecureRandom;
 
 public class Main {
-    //segundo cambio
+    //prueba
     public static void main(String[] args) {
         SimpleRandomProvider secureRandom = new SimpleRandomProvider();
         SaltGenerator saltGenerator = new SaltGenerator(secureRandom);
